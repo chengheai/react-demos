20428ec2d70e0df2a7e08a9f142e355a289945cf
chengheai <842385139@qq.com>
2020-02-26 00:07:19 +0800

    wenguerzhixin

--- demo1/src/App.js
+++ demo1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button, {Nav} from './Button';
+import Button, { Nav } from './Button';
 import Input from './Input';
 // 类方式
 // class Nav extends Component{
@@ -19,36 +19,38 @@ import Input from './Input';
 //   )
 // }
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
     this.state = {
-      like: false
-    }
+      like: false,
+    };
   }
-  handleClick(){
+  handleClick() {
     this.setState({
-      like: !this.state.like
-    })
+      like: !this.state.like,
+    });
   }
-  render(){
-    console.log('render函数触发了！！')
-    const {like} = this.state;
+  render() {
+    console.log('render函数触发了！！');
+    const { like } = this.state;
     return (
       <div>
         <Nav />
         <Button></Button>
         <Input></Input>
-        <button type='button' style={like ? {color: 'red'}: {color: 'black'}}
-          onClick={() =>this.handleClick()}
+        <button
+          type="button"
+          style={like ? { color: 'red' } : { color: 'black' }}
+          onClick={() => this.handleClick()}
         >
           {like ? '已赞' : '喜欢'}
         </button>
-        <hr/>
+        <hr />
         <Nav title={'标题'}>
           <h1>这是props.children</h1>
           <h3>这是一个props传递的1</h3>
-          <Button name='test1111'></Button>
-          <Button name='121212121212'></Button>
+          <Button name="test1111"></Button>
+          <Button name="121212121212"></Button>
         </Nav>
       </div>
     );
